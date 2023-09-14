e7bb7bf5665ae6d56a3c5bb8aba404742a98b581
Lavesh12 <94188971+Lavesh12@users.noreply.github.com>
2023-09-14 12:02:28 +0530

    Add files via upload

---
+++ src/components/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="hero border-1 pb-3">
+        <div className="card bg-dark text-white border-0 mx-3">
+          <img
+            className="card-img img-fluid"
+            src="./assets/main.png"
+            alt="Card"
+            height={500}
+          />
+          <div className="card-img-overlay d-flex align-items-center">
+            <div className="container">
+              <b className="card-title fs-5 text-center fw-lighter"></b>
+              <p className="card-text fs-5 d-none d-sm-block ">
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
